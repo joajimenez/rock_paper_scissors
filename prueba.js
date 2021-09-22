73661495312068a09651c8967f1a402c3960b783
Joan Jimenez <jimenez.donaldson@gmail.com>
2021-09-22 06:35:03 -0400

    init start

---
+++ prueba.js
@@ -0,0 +1,22 @@
+function computerPlay (){
+Math.random()
+    if Math.random()
+}
+
+const playerSelection = prompt('Rock, Paper or Scissors?')
+
+//single round function
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection = "rock"
+}
+
+const playerSelection = "rock", 
+const computerSelection = computerPlay()
+
+//Game Function
+
+function game(){
+
+}
