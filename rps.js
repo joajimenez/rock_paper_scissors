4c17046f48a2a5435329cdc90260fd2fe7dd73a8
Joan Jimenez <jimenez.donaldson@gmail.com>
2021-09-22 11:25:12 -0400

    added computerPlay logic.

--- rps.js
+++ rps.js
@@ -1,22 +1,29 @@
-function computerPlay (){
-Math.random()
-    if Math.random()
+//Computer Selection
+
+let computerPlay = Math.random()
+
+if (computerPlay < 0.34) {
+    computerPlay = 'Rock'
+} else if (computerPlay  <= 0.63) {
+    computerPlay = 'Paper'
+} else {
+computerPlay = 'Scissors'
 }
 
+console.log(computerPlay ) 
+
+//playerSelection
 const playerSelection = prompt('Rock, Paper or Scissors?')
 
 //single round function
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {}
 
-    if (playerSelection = "rock"
-}
-
-const playerSelection = "rock", 
+const playerSelection = "",
 const computerSelection = computerPlay()
 
 //Game Function
 
-function game(){
+function game() {
 
 }
