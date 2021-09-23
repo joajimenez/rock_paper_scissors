c041aac6c2944f9c78a5e5304a2a8e4840067563
Joan Jimenez <jimenez.donaldson@gmail.com>
2021-09-23 14:37:10 -0400

    Refactored playRound code.

--- rps.js
+++ rps.js
@@ -1,29 +1,67 @@
+//playerSelection
+let playerSelection = prompt("Rock, Paper or Scissors?");
+
 //Computer Selection
 
-let computerPlay = Math.random()
-
-if (computerPlay < 0.34) {
-    computerPlay = 'Rock'
-} else if (computerPlay  <= 0.63) {
-    computerPlay = 'Paper'
-} else {
-computerPlay = 'Scissors'
+function computerPlay() {
+  const cp = Math.random();
+  if (cp < 0.34) {
+    return "Rock";
+  } else if (cp <= 0.63) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
-console.log(computerPlay ) 
-
-//playerSelection
-const playerSelection = prompt('Rock, Paper or Scissors?')
+//Computer's Selection Console log
+console.log("The robot selected " + computerPlay());
+console.log("You selected " + playerSelection);
 
 //single round function
 
-function playRound(playerSelection, computerSelection) {}
 
-const playerSelection = "",
-const computerSelection = computerPlay()
+
+//Computer Selection
+
+
+//Play Round Function
+
+function playRound(ps, cs) {
+  console.log(ps, cs)
+  if (ps.toLowerCase() == "paper" && cs == "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (
+    ps.toLowerCase() == "paper" &&
+    cs == "Scissors"
+  ) {
+    return "You Lose! Scissors beats Paper";
+  } else if (
+    ps.toLowerCase() == "rock" &&
+    cs == "Scissors"
+  ) {
+    return "You Win! Rock beats Scissors";
+  } else if (
+    ps.toLowerCase() == "rock" &&
+    cs == "Paper"
+  ) {
+    return "You Lose! Paper beats Rock";
+  } else if (
+    ps.toLowerCase() == "scissors" &&
+    cs == "Paper"
+  ) {
+    return "You Win! Scissors beats Paper";
+  } else if (
+    ps.toLowerCase() == "scissors" &&
+    cs == "Rock"
+  ) {
+    return "You lose! Rock beats Scissors";
+  } else {
+  console.log(ps.toLowerCase(), cs, 'finally')
+    return "Is a tie!";
+  }
+}
+
+console.log(playRound(playerSelection, computerPlay()));
 
 //Game Function
-
-function game() {
-
-}
