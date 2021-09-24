5297e777efb63ebaeea5b4a88a7d1fa9e0dadf4f
Joan Jimenez <jimenez.donaldson@gmail.com>
2021-09-24 18:32:16 -0400

    refactored game logic

--- rps.js
+++ rps.js
@@ -1,67 +1,59 @@
-//playerSelection
-let playerSelection = prompt("Rock, Paper or Scissors?");
+//Human's selection
+let playerSelection = prompt("Choose wisely: Rock, Paper or Scissors?");
 
-//Computer Selection
+//Computer's Selection
 
-function computerPlay() {
+function computerSelection() {
   const cp = Math.random();
-  if (cp < 0.34) {
+  if (cp <= 0.33) {
     return "Rock";
-  } else if (cp <= 0.63) {
+  } else if (cp >= 34 || cp <= 66) {
     return "Paper";
   } else {
     return "Scissors";
   }
 }
 
-//Computer's Selection Console log
-console.log("The robot selected " + computerPlay());
-console.log("You selected " + playerSelection);
-
-//single round function
-
-
-
-//Computer Selection
-
-
 //Play Round Function
 
 function playRound(ps, cs) {
-  console.log(ps, cs)
-  if (ps.toLowerCase() == "paper" && cs == "Rock") {
+  console.log(
+    "You selected " + ps.toUpperCase() + " and the computer selected " + cs
+  );
+
+  if (ps != "paper" && ps != "scissors" && ps != "rock") {
+    return "Good at spelling, you are not.";
+  } else if (ps.toLowerCase() == "paper" && cs == "Rock") {
     return "You Win! Paper beats Rock";
-  } else if (
-    ps.toLowerCase() == "paper" &&
-    cs == "Scissors"
-  ) {
+  } else if (ps.toLowerCase() == "paper" && cs == "Scissors") {
     return "You Lose! Scissors beats Paper";
-  } else if (
-    ps.toLowerCase() == "rock" &&
-    cs == "Scissors"
-  ) {
+  } else if (ps.toLowerCase() == "rock" && cs == "Scissors") {
     return "You Win! Rock beats Scissors";
-  } else if (
-    ps.toLowerCase() == "rock" &&
-    cs == "Paper"
-  ) {
+  } else if (ps.toLowerCase() == "rock" && cs == "Paper") {
     return "You Lose! Paper beats Rock";
-  } else if (
-    ps.toLowerCase() == "scissors" &&
-    cs == "Paper"
-  ) {
+  } else if (ps.toLowerCase() == "scissors" && cs == "Paper") {
     return "You Win! Scissors beats Paper";
-  } else if (
-    ps.toLowerCase() == "scissors" &&
-    cs == "Rock"
-  ) {
+  } else if (ps.toLowerCase() == "scissors" && cs == "Rock") {
     return "You lose! Rock beats Scissors";
   } else {
-  console.log(ps.toLowerCase(), cs, 'finally')
+    console.log(ps.toUpperCase(), cs.toUpperCase(), "finally");
     return "Is a tie!";
   }
 }
 
-console.log(playRound(playerSelection, computerPlay()));
+console.log(playRound(playerSelection, computerSelection()));
 
 //Game Function
+
+function game() {
+  console.log(playRound(playerSelection, computerSelection()));
+  console.log(playRound(playerSelection, computerSelection()));
+  console.log(playRound(playerSelection, computerSelection()));
+  console.log(playRound(playerSelection, computerSelection()));
+  console.log(playRound(playerSelection, computerSelection()));
+}
+
+let playerPoints;
+let robotPoints;
+
+//function scoreTracker (_pp, _rp) {
