9aca444ea99c84ef5d39c5fa8f967463b8241d72
Joan Jimenez <jimenez.donaldson@gmail.com>
2021-11-03 15:32:49 -0400

    added points tracker

--- rps.js
+++ rps.js
@@ -1,16 +1,21 @@
+let playerPoints = 0;
+let robotPoints = 0;
+let draw = 0;
+
 //Human's selection
-let playerSelection = prompt("Choose wisely: Rock, Paper or Scissors?");
+let playerSelection = prompt('Choose wisely: Rock, Paper or Scissors?');
 
 //Computer's Selection
 
 function computerSelection() {
   const cp = Math.random();
   if (cp <= 0.33) {
-    return "Rock";
+    return 'Rock';
+    ``;
   } else if (cp >= 34 || cp <= 66) {
-    return "Paper";
+    return 'Paper';
   } else {
-    return "Scissors";
+    return 'Scissors';
   }
 }
 
@@ -18,30 +23,39 @@ function computerSelection() {
 
 function playRound(ps, cs) {
   console.log(
-    "You selected " + ps.toUpperCase() + " and the computer selected " + cs
+    'You selected ' + ps.toUpperCase() + ' and the computer selected ' + cs
   );
 
-  if (ps != "paper" && ps != "scissors" && ps != "rock") {
-    return "Good at spelling, you are not.";
-  } else if (ps.toLowerCase() == "paper" && cs == "Rock") {
-    return "You Win! Paper beats Rock";
-  } else if (ps.toLowerCase() == "paper" && cs == "Scissors") {
-    return "You Lose! Scissors beats Paper";
-  } else if (ps.toLowerCase() == "rock" && cs == "Scissors") {
-    return "You Win! Rock beats Scissors";
-  } else if (ps.toLowerCase() == "rock" && cs == "Paper") {
-    return "You Lose! Paper beats Rock";
-  } else if (ps.toLowerCase() == "scissors" && cs == "Paper") {
-    return "You Win! Scissors beats Paper";
-  } else if (ps.toLowerCase() == "scissors" && cs == "Rock") {
-    return "You lose! Rock beats Scissors";
+  if (ps != 'paper' && ps != 'scissors' && ps != 'rock') {
+    return 'Good at spelling, you are not.';
+  } else if (ps.toLowerCase() == 'paper' && cs == 'Rock') {
+    playerPoints++;
+    return 'You Win! Paper beats Rock';
+  } else if (ps.toLowerCase() == 'paper' && cs == 'Scissors') {
+    robotPoints++;
+    return 'You Lose! Scissors beats Paper';
+  } else if (ps.toLowerCase() == 'rock' && cs == 'Scissors') {
+    playerPoints++;
+    return 'You Win! Rock beats Scissors';
+  } else if (ps.toLowerCase() == 'rock' && cs == 'Paper') {
+    robotPoints++;
+    return 'You Lose! Paper beats Rock';
+  } else if (ps.toLowerCase() == 'scissors' && cs == 'Paper') {
+    playerPoints++;
+    return 'You Win! Scissors beats Paper';
+  } else if (ps.toLowerCase() == 'scissors' && cs == 'Rock') {
+    robotPoints++;
+    return 'You lose! Rock beats Scissors';
   } else {
-    console.log(ps.toUpperCase(), cs.toUpperCase(), "finally");
-    return "Is a tie!";
+    draw++;
+    return ps.toUpperCase(), cs.toUpperCase(), 'Is a tie!';
   }
 }
 
 console.log(playRound(playerSelection, computerSelection()));
+console.log(
+  `Player Points = ${playerPoints}, Computer Points = ${robotPoints}, and Draws = ${draw}`
+);
 
 //Game Function
 
@@ -52,8 +66,5 @@ function game() {
   console.log(playRound(playerSelection, computerSelection()));
   console.log(playRound(playerSelection, computerSelection()));
 }
-
-let playerPoints;
-let robotPoints;
 
 //function scoreTracker (_pp, _rp) {
